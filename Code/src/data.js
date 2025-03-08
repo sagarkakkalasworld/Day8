6564b1c46f8ccddc63bb481b841f7b57a0044735
sagar <sagar@sagars-MacBook-Air.local>
2025-03-08 10:07:49 +0000

    removed helm files

--- Code/src/data.js
+++ Code/src/data.js
@@ -2,7 +2,7 @@ export const sliderItems = [
   {
     id: "1",
     img: "assets/braclet/main.jfif",
-    title: "SSL has been configured successfully",
+    title: "Helm charts have been successfully configured",
     desc: "This is now in new commit",
     bg: "green",
   },
