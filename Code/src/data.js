2702843086c0932bf31f730b892dce23885e7c20
sagarkakkalasworld <sagarkakkalasworld@gmail.com>
2025-03-08 11:16:27 +0000

    Update data.js

--- Code/src/data.js
+++ Code/src/data.js
@@ -2,7 +2,7 @@ export const sliderItems = [
   {
     id: "1",
     img: "assets/braclet/main.jfif",
-    title: "Helm charts have been successfully configured",
+    title: "Helm charts have been successfully configured for our Application",
     desc: "This is now in new commit",
     bg: "green",
   },
